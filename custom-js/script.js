a2cbe2e2319156ff77bf51f9a1c4cd24841c0a3f
Jijinet <jihanrabat345@gmail.com>
2021-07-05 00:51:06 +0100

    project templates

---
+++ custom-js/script.js
@@ -0,0 +1,186 @@
+$( document ).ready(function() {
+  
+    const editProfile=$('#edit-profile');
+    const saveProfile=$('#save-profile');
+
+    const addContent=$('#add-content');
+    const addContent2=$('#add-content2');
+    const addCourse=$('#add-course');
+
+    const profileDescription=$('#profile-description');
+    const profileForm=$('#profile-form');
+
+    const contentForm=$('#content-form');
+    const addContentContainer=$('#add-content-container');
+    const contentContainer=$('#content-container');
+
+    $(editProfile).on('click',()=>{
+      profileDescription.addClass('uk-hidden');
+      profileForm.removeClass('uk-hidden');
+      UIkit.switcher('.dashboard-tab').show(0);
+    });
+
+    $(saveProfile).on('click',()=>{
+        profileForm.addClass('uk-hidden');
+        profileDescription.removeClass('uk-hidden');
+      });
+
+      $(addContent).on('click',()=>{
+        addContentContainer.addClass('uk-hidden');
+        contentContainer.addClass('uk-hidden');
+        contentForm.removeClass('uk-hidden');
+        UIkit.switcher('.dashboard-tab').show(2);  
+      });
+
+      $(addContent2).on('click',()=>{
+        addContentContainer.addClass('uk-hidden');
+        contentContainer.addClass('uk-hidden');
+        contentForm.removeClass('uk-hidden');
+      });
+
+      $(addCourse).on('click',()=>{
+        contentForm.addClass('uk-hidden');
+        contentContainer.removeClass('uk-hidden');
+      });
+
+
+      // ! content table
+
+      const addContentTable=$('#add-content-table');
+      
+      const contentInfoContainer=$('#content-info-container');
+      const contentTableContainer=$('#content-table-container');
+
+    
+
+     
+      
+      $(addContentTable).on('click',()=>{
+
+        let inputTitle=$('#title-content').val();
+        let inputDuration=$('#duration-content').val();
+        let inputUrl=$('#video').val();
+
+        contentTableContainer.removeClass('uk-hidden');
+        
+        contentInfoContainer.append(`<tr id="content-element">
+        <td>${inputTitle}</td>
+        <td>${inputUrl}</td>
+        <td>${inputDuration}</td>
+        <td><a uk-icon="icon: trash;" id="remove-content"
+                class="uk-text-danger uk-align-right"></a></td>
+    </tr>`);
+// todo
+        inputTitle=$('#title-content').val('');
+        inputDuration=$('#duration-content').val('');
+        inputUrl=$('#video').val('');
+
+      });
+   
+      $(document).on('click','#remove-content',()=>{
+        console.log('btn clicked');
+        $('#content-element').remove();
+        if(contentInfoContainer.children().length > 0 ){
+          contentTableContainer.removeClass('uk-hidden');
+        }
+   
+      });
+
+      $('#category-name').html($('#categorie-list ul li a').first().html());
+
+
+      $(document).on('click','#categorie-list ul li',function(){
+        let categoryName=$('#category-name');
+        
+        let categoryValue=$(this).children().html();
+        $(categoryName).html(categoryValue);
+     
+        $('#categorie-list ul li').removeClass('active');
+        $('#categorie-list ul li a').removeClass('active');
+        $('#categorie-list ul li span').removeClass('active');
+    
+        $(this).addClass('active');
+        $(this).children().addClass('active');
+    
+      });
+
+      $(".search-form").on('submit',function(e) {
+        e.preventDefault();
+        let searchName=$('#search-name');
+        let searchValue=$('.search-value').val();
+        $(searchName).html(searchValue);
+        
+    });
+
+    //! Modal Switch
+
+    const signinBtn=$('.signin-btn');
+    const signupBtn=$('.signup-btn');
+
+    const signupContainer=$('.signup-container');
+    const signinContainer=$('.signin-container');
+
+
+    $(signinBtn).on('click',()=>{
+        signinContainer.removeClass('uk-hidden');
+        signupContainer.addClass('uk-hidden');
+        console.log('clicked');
+    });
+
+    $(signupBtn).on('click',()=>{
+      signupContainer.removeClass('uk-hidden');
+      signinContainer.addClass('uk-hidden');
+      console.log('clicked');
+ 
+    });
+
+    
+    // ! delete instructor element
+
+    const deleteBtn = $('.delete-btn');
+
+    const instructorElement=$('#instructor-element');
+
+   
+    $(deleteBtn).on('click',()=>{
+      if($('.checkbox-table').is(':checked')){
+
+       $(instructorElement).remove();
+
+      }
+ 
+    });
+
+    
+
+
+  });
+ 
+
+  function iconClick(e,id){
+
+    const approvedIcon = $('#approved-icon_'+id);
+    const disapprovedIcon = $('#disapproved-icon_'+id);
+
+    const permissionBtn=$('#permission-btn_'+id);
+
+    
+    $(approvedIcon).data('clicked',true);    
+    $(disapprovedIcon).data('clicked',true);
+
+    if($(disapprovedIcon).data('clicked')){
+      permissionBtn.html('Disapproved');
+    
+    }
+
+    else if($(approvedIcon).data('clicked')){
+      permissionBtn.html('Approved');
+    }
+
+
+    $(disapprovedIcon).remove();
+    $(approvedIcon).remove();
+
+  }
+   
+  const player = new Plyr('#player');
